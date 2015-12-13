54a94b46ea041d378f55f89bfbecd9111c2d6aff
ellis <byskyline2158@gmail.com>
2015-12-13 10:45:02 +0800

    git "kaminari" flash

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,16 @@
 class EventsController < ApplicationController
 
+  before_action :set_event, :only => [ :show, :edit, :update, :destroy]
 
   #GET /events/index
   #GET /events
   def index
-    @events = Event.all
+    @events = Event.page( params[:page]).per(10)
   end
 
   #GET /events/show/:id
   def show
-    @event = Event.find( params[:id])
+    @page_title = @event.bookname
   end
 
   #GET /events/new
@@ -21,37 +22,43 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
 
-    @event.save
+    if @event.save
+      flash[:notice] = "新增成功"
+      redirect_to :action => :index #告訴瀏覽器  HTTP code:303
+    else
+      render :action => :new #new.html.erb
+    end
 
-    redirect_to :action => :index #告訴瀏覽器  HTTP code:303
   end
 
   #GET /events/edit/:id
   def edit
-    @event = Event.find( params[:id])
   end
 
   #POST /events/update/:id
   def update
-    @event = Event.find(event_params)
-
-    @event.update(event_params)
-
-    redirect_to :action => :show, :id => @event
+    if @event.update(event_params)
+      flash[:notice] = "編輯成功"
+      redirect_to :action => :show, :id => @event
+    else
+      render :action => :edit # edit.html.erb
+    end
   end
 
   #GET /events/destory/id
-  def destory
-    @event = Event.find(event_params)
-
-    @event.destory
-
+  def destroy
+    @event.destroy
+    flash[:alert] = "刪除成功"
     redirect_to :action => :index
   end
 
 
 
   private
+
+  def set_event
+    @event = Event.find( params [:id])
+  end
 
   def event_params
     params.require(:event).permit(:bookname,:name,:price)
